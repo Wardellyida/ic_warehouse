fea6b55e640c479f7516b2dba6bea9d811a90e9f
YIDA CHEN <70647908+Wardellyida@users.noreply.github.com>
2024-01-06 23:25:20 +1100

    login password changed

--- database query.sql
+++ database query.sql
@@ -4,3 +4,8 @@ select * from ic_warehouse.managers;
 
 insert into ic_warehouse.managers
 	   values (01, "Manager", "IC2062060");
+       
+update ic_warehouse.managers
+set password="IC206206"
+where username="Manager";
+
